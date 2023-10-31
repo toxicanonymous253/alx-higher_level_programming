788a7be1caf51e46215f1e6236433d424f3d51eb
toxicfrank <ngugifrancis930@gmai.com>
2023-10-31 07:30:20 +0300

    alx

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(f"{str[39:66]} with Python")
+print(f"{str[39:67]}with Python")
