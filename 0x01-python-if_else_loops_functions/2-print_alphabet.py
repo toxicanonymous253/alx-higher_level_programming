7680fb9ff3659646e4bb2f026b8587e7df8c478d
toxicfrank <ngugifrancis930@gmai.com>
2023-10-31 14:49:14 +0300

    alx

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end="")
+for letter in range(97, 23):
+    print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 alphabet = ""
-for letter in range(ord('a'), ord('z') + 1):
+for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
-        alphabet += chr(letter)
-print(alphabet, end="")
+        print("{}".format(chr(letter)), end="")
