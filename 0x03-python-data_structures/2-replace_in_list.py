5947ccbb99c79a251b603ea6190963d29c44f700
toxicfrank <ngugifrancis930@gmai.com>
2023-11-04 13:06:56 +0300

    alx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if not my_list:
+        return my_list
     list_size = len(my_list)
     if idx < 0:
         return my_list
-    if idx > list_size:
+    elif idx > list_size:
         return my_list
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
