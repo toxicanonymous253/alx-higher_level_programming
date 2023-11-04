7fabe8e761bacee6b6988702640e909f71ee55a4
toxicfrank <ngugifrancis930@gmai.com>
2023-11-04 13:01:09 +0300

    alx

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
-    for num in range(len(my_list)):
-        print("{}".format(my_list[num]))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
