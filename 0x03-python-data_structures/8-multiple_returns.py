f39aa3ab8b893dc2a1fbfe2fa4bbefc096def52a
toxicfrank <ngugifrancis930@gmai.com>
2023-11-06 19:19:22 +0300

    alx

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 def multiple_returns(sentence):
     if sentence is None:
         return 0, None
-    lengthofsentence = len(sentence)
-    firstletter = sentence[0]
-    return lengthofsentence, firstletter
+    else:
+        lengthofsentence = len(sentence)
+        firstletter = sentence[0]
+        return lengthofsentence, firstletter
