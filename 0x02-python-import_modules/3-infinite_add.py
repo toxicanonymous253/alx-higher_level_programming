4845efdafce12a0f2adc6ed6711343e2b6a74b74
toxicfrank <ngugifrancis930@gmai.com>
2023-11-07 09:16:45 +0300

    alx

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-if len(sys.argv) == 1:
-    print("0")
-if len(sys.argv) > 2:
-    for i in range(1, len(sys.argv)):
-        result = int(sys.argv[i])
-        sum += result
-print("{:d}".format(sum))
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    if len(sys.argv) == 1:
+        print("0")
+    if len(sys.argv) > 2:
+        for i in range(1, len(sys.argv)):
+            result = int(sys.argv[i])
+            sum += result
+    print("{:d}".format(sum))
