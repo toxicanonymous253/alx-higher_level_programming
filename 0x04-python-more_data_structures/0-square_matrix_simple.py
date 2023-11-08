274a8e9bf8269c0531edf13568eb47289cc704b9
toxicfrank <ngugifrancis930@gmai.com>
2023-11-08 13:15:59 +0300

    alx

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    duplicat_matrix = []
-    for row in matrix:
-        duplicat_row = []
-        for element in row:
-            duplicat_row.append(element ** 2)
-        duplicat_matrix.append(duplicat_row)
-    return duplicat_matrix
+    return [[i * i for i in row] for row in matrix]
