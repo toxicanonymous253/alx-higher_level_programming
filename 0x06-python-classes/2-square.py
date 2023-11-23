36aeb0ebe7ee0de62c178d331d1e6b3406355d8d
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-11-23 02:57:23 -0800

    alx

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defination of a class Square"""
+
+
+class Square:
+    """Represents a new square
+    Args:
+        size(int): The size of the new square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
