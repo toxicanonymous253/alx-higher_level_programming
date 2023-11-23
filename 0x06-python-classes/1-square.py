16729e7f180ad711cb385bb6d9c591581b8fa81d
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-11-23 02:33:24 -0800

    alx

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Define a class Square"""
 class Square:
+    """Represents a new square
+    Args:
+        size(int): The size of the new square
+    """
     def __init__(self, size):
         self.__size = size
