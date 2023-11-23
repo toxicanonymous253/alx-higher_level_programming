8e356f260ba15a0c6ef42024cf118fba895d856c
toxicanonymous253 <ngugifrancis930@gmail.com>
2023-11-23 03:27:29 -0800

    alx

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,9 +9,11 @@ class Square:
     """
     def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
